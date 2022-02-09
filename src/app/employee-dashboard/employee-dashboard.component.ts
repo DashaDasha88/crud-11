9ca6770f787164c7c194feffcf43c71f1438a47d
Dasha <dasha.kotova.mail@gmail.com>
2022-02-09 17:47:57 -0500

    add conditional display of add and edit buttons

--- src/app/employee-dashboard/employee-dashboard.component.ts
+++ src/app/employee-dashboard/employee-dashboard.component.ts
@@ -13,6 +13,8 @@ export class EmployeeDashboardComponent implements OnInit {
   formValue !: FormGroup;
   employeeModelObj : EmployeeModel = new EmployeeModel();
   employeeData !: any;
+  showAdd !: boolean;
+  showUpdate !: boolean;
 
   constructor(private formbuilder: FormBuilder, private api : ApiService) { }
 
@@ -26,6 +28,12 @@ export class EmployeeDashboardComponent implements OnInit {
     })
 
     this.getAllEmployees();
+  }
+
+  clickAdd(){
+    this.formValue.reset();
+    this.showAdd = true;
+    this.showUpdate = false;
   }
 
   postEmployeeDetails(){
@@ -65,11 +73,33 @@ export class EmployeeDashboardComponent implements OnInit {
   }
 
   onEdit(row : any) {
+    this.showAdd = false;
+    this.showUpdate = true;
+
+    this.employeeModelObj.id = row.id;
+
     this.formValue.controls['firstName'].setValue(row.firstName);
     this.formValue.controls['lastName'].setValue(row.lastName);
     this.formValue.controls['email'].setValue(row.email);
     this.formValue.controls['phone'].setValue(row.phone);
     this.formValue.controls['salary'].setValue(row.salary);
+  }
+
+  updateEmployeeDetails(){
+    this.employeeModelObj.firstName = this.formValue.value.firstName;
+    this.employeeModelObj.lastName = this.formValue.value.lastName;
+    this.employeeModelObj.email = this.formValue.value.email;
+    this.employeeModelObj.phone = this.formValue.value.phone;
+    this.employeeModelObj.salary = this.formValue.value.salary;
+
+    this.api.updateEmployee(this.employeeModelObj, this.employeeModelObj.id)
+    .subscribe(res => {
+      alert("Updated successfully!");
+      let ref = document.getElementById('cancel');
+      ref?.click();
+      this.formValue.reset();
+      this.getAllEmployees();
+    })
   }
 
 }
