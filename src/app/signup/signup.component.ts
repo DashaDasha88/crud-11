d80ebb8c4270ed676d37f47b59da3462ae43dd3c
Dasha <dasha.kotova.mail@gmail.com>
2022-02-10 20:50:29 -0500

    add signup and login

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -10,7 +12,7 @@ export class SignupComponent implements OnInit {
 
   public signupForm !: FormGroup;
 
-  constructor(private formBuilder : FormBuilder) { }
+  constructor(private formBuilder : FormBuilder, private http : HttpClient, private router : Router) { }
 
   ngOnInit(): void {
     this.signupForm = this.formBuilder.group({
@@ -18,6 +20,17 @@ export class SignupComponent implements OnInit {
       email: [''],
       password: [''],
       phone: [''],
+    })
+  }
+
+  signUp(){
+    this.http.post<any>("http://localhost:3000/signupUsers", this.signupForm.value)
+    .subscribe(res => {
+      alert("Signup Successful");
+      this.signupForm.reset();
+      this.router.navigate(['login']);
+    }, err => {
+      alert("Something went wrong");
     })
   }
 
